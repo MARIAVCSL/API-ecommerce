5cd28a2cca3794e2a2ab3d42106712c29b627d98
Maria Eduarda Santos Ferreira <eduarda.santos16042007@gmail.com>
2024-12-16 22:14:30 -0300

    alteração

---
+++ front/scripts/database.js
@@ -0,0 +1,217 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+    {
+        codigoProduto:1,
+        tituloProduto:"Formato cenoura", 
+        preco: 29.40, 
+        descricao:"Surpreenda seu cão com um petisco da Azul Dog, preparado com ingredientes de qualidade. Sua textura macia e flexível é irresistível, tornando cada momento especial cheio de sabor e diversão. Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa. ",
+        imagemProduto: {
+            img1:"./images/cenoura.webp",
+            img2:"./images/cenoura2.webp",
+            img3:"./images/cenoura3.webp",
+            img4:"./images/cenoura4.webp",
+
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:2,
+        tituloProduto:"Orelha suína desidratada", 
+        preco: 70.80, 
+        descricao:"Produto indicado para cães de porte P/M/G, produto desidratado naturalmente, Livre de conservantes, ingredientes selecionados, atrativo, saboroso, alivia o estresse canino, estimula a masticação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa. ",
+        imagemProduto: {
+            img1:"./images/orelha.webp",
+            img2:"./images/orelha2.webp",
+            img3:"./images/orelha3.webp",
+    },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:3,
+        tituloProduto:"Bisco osso sabor chocolate", 
+        preco: 29.40, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa. ",
+        imagemProduto: {
+            img1:"./images/ossochocolate.webp",
+            img2:"./images/ossochocolate2.webp",
+            img3:"./images/ossochocolate3.webp",
+
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:4,
+        tituloProduto:"Trança flex sabor menta", 
+        preco: 29.40, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa.",
+        imagemProduto: {
+            img1:"./images/ossoverde.webp",
+            img2:"./images/ossoverde1.webp",
+            img3:"./images/ossoverde2.webp",
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:5,
+        tituloProduto:"Reggae", 
+        preco: 29.40, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa.",
+        imagemProduto: {
+            img1:"./images/reggae.webp",
+            img2:"./images/reggae2.webp",
+            img3:"./images/reggae3.webp",
+            img4:"./images/reggae4.webp",
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:6,
+        tituloProduto:"Palito Fino", 
+        preco: 20.30, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa.",
+        imagemProduto: {
+            img1:"./images/PalitoFino4.webp",
+            img2:"./images/PalitoFino2.webp",
+            img3:"./images/PalitoFino3.webp",
+            img4:"./images/PalitoFino4.webp",
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:7,
+        tituloProduto:"Tubets", 
+        preco: 39.90, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa.",
+        imagemProduto: {
+            img1:"./images/Tubets.webp",
+            img2:"./images/Tubets2.webp",
+            img3:"./images/Tubets3.webp",
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+  
+    {
+        codigoProduto:8,
+        tituloProduto:"Palito de Frango", 
+        preco: 29.40, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa.",
+        imagemProduto: {
+            img1:"./images/PalitoFrango.webp",
+            img2:"./images/PalitoFrango2.webp",
+            img3:"./images/PalitoFrango3.webp",
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+  
+   
+
+    {
+        codigoProduto:9,
+        tituloProduto:"Pulmão Bovino desidratado", 
+        preco: 5.60, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa. ",
+        imagemProduto: {
+            img1:"./images/pulmao.webp",
+            img2:"./images/pulmao2.webp",
+            img3:"./images/pulmao3.webp",
+
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:10,
+        tituloProduto:"Nó gourmet 3x4 com batata doce", 
+        preco: 30.80, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa.",
+        imagemProduto: {
+            img1:"./images/batata.webp",
+            img2:"./images/batata2.webp",
+            img3:"./images/batata3.webp",
+            img4:"./images/batata4.webp",
+
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:11,
+        tituloProduto:"Trança reggae", 
+        preco: 29.40, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa.",
+        imagemProduto: {
+            img1:"./images/reggae.webp",
+            img2:"./images/reggae2.webp",
+            img3:"./images/reggae3.webp",
+            img4:"./images/reggae4.webp",
+
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:12,
+        tituloProduto:"Loop mix", 
+        preco: 29.40, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa. ",
+        imagemProduto: {
+            img1:"./images/Loop.webp",
+            img2:"./images/Loop2.webp",
+            img3:"./images/Loop3.webp",
+            img4:"./images/Loop4.webp",
+
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:13,
+        tituloProduto:"Orelha suína desidratada", 
+        preco: 23.40, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa. ",
+        imagemProduto: {
+            img1:"./images/orelha.webp",
+            img2:"./images/orelha2.webp",
+            img3:"./images/orelha3.webp",
+
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },    {
+        codigoProduto:14,
+        tituloProduto:"Orelha suína desidratada", 
+        preco: 27.40, 
+        descricao:"Produto indicado para cães de porte P/M/G, contém conservantes naturais a base de alecrim, ingredientes selecionados, contém aveia que auxilia na digestão, composto vitamínico, hexametafosfato que ajuda na prevenção de tártaros, atrativo, saboroso, alivia o estresse canino, estimula a mastigação, favorece o enriquecimento ambiental promovendo saúde e bem-estar, utilizado como agrado e recompensa. ",
+        imagemProduto: {
+            img1:"./images/orelha.webp",
+            img2:"./images/orelha2.webp",
+            img3:"./images/orelha3.webp",
+
+        },
+        categoriaProduto: 'Petiscos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    
+    
+]
